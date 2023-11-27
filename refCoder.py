95caeb14830369875e6f259723db8ce8e042e8de
komca12 <komca12@gmail.com>
2023-11-27 07:23:16 +0100

    ref coder and encoder

--- refCoder.py
+++ refCoder.py
@@ -1,20 +1,30 @@
 import numpy as np
+from pyldpc import make_ldpc, encode
 
-from pyldpc import make_ldpc, encode, decode, get_message
-
+def string_to_binary_row(input_string):
+    binary_row = []
+    for char in input_string:
+        binary_representation = bin(ord(char))[2:].zfill(8)  # Převod na binární reprezentaci
+        binary_row.extend([int(bit) for bit in binary_representation])
+    return binary_row
 
 inStr =  input()
 
-n = len(inStr)*2
-d_v = len(inStr)-1
-d_c = len(inStr)
-snr = 20
-H, G = make_ldpc(n, d_v, d_c, systematic=True, sparse=True)
-k = G.shape[1]
-v = np.random.randint(2, size=k)
-y = encode(G, v, snr)
-d = decode(H, y, snr)
-x = get_message(G, d)
-assert abs(x - v).sum() == 0
 
-print(x)
+bit_matrix = string_to_binary_row(inStr)
+inStrLen= len(bit_matrix)
+
+n = inStrLen*2 
+d_v = inStrLen-1
+d_c = inStrLen
+snr = 20
+
+_, G = make_ldpc(n, d_v, d_c,seed=42)
+
+y = encode(G, bit_matrix, snr)
+
+codedMassage= (y > 0).astype(int)
+
+for i in codedMassage:
+    print(i, end="")
+print()
